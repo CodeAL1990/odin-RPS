962817e1f7b792b88c3440e62910ab13748e6281
FetusCoder <127114506+FetusCoder@users.noreply.github.com>
2023-04-03 14:41:03 +0700

    linked script.js and planned outline

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-RPS
 basic rps game
+
+1. Basic outline of what i think at first using comments
---
+++ script.js
@@ -0,0 +1,8 @@
+//Initialize variable for user1
+//Initialize variable for user2
+//user1 picks an option from rock, paper and scissors
+//user2 picks *randomly from option rock, paper and scissors
+//if user1 and user2 picks the same option, it's a draw
+//if user1 picks rock and user 2 picks paper and vice versa, paper wins
+//if user1 picks paper and user 2 picks scissors and vice versa, scissors wins
+//if user1 picks scissors and user 2 picks rock and vice versa, rock wins
